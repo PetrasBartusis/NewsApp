829ea6dfecb969d767197c30a0c7d66e1f4c2db8
PetrasBartusis <bartusiuks@gmail.com>
2018-04-02 13:27:26 +0300

    Create packages for different features.

--- app/src/main/java/com/example/bartusis/petras/newsapp/main/main/newsList/MainActivity.kt
+++ app/src/main/java/com/example/bartusis/petras/newsapp/main/main/newsList/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.bartusis.petras.newsapp.main.main
+package com.example.bartusis.petras.newsapp.main.main.newsList
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
