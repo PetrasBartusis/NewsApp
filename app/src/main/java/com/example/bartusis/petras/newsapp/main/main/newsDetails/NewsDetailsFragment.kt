e57f955b2b26b6b2c28c257ecdfa4537855da0d6
PetrasBartusis <bartusiuks@gmail.com>
2018-04-08 12:51:23 +0300

    Changed layout padding.

--- app/src/main/java/com/example/bartusis/petras/newsapp/main/main/newsDetails/NewsDetailsFragment.kt
+++ app/src/main/java/com/example/bartusis/petras/newsapp/main/main/newsDetails/NewsDetailsFragment.kt
@@ -31,20 +31,19 @@ class NewsDetailsFragment : BaseFragment(), NewsDetailsContract.View {
         val article = arguments.getSerializable(Article.ARTICLE) as Article
         GlideImageLoader().loadFullImage(article.urlToImage.toString(), top_image, activity.windowManager)
         setArticleText(article)
-        article_link_btn.setOnClickListener { openBrowserWithUrl(article)}
+        article_link_btn.setOnClickListener { openBrowserWithUrl(article) }
     }
 
-    private fun openBrowserWithUrl(article: Article){
+    private fun openBrowserWithUrl(article: Article) {
         val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(article.url.toString()))
         startActivity(browserIntent)
     }
 
-    private fun setArticleText(article: Article){
+    private fun setArticleText(article: Article) {
         article_title.text = article.title
         article_description.text = article.description
         article_author.text = article.author
-        article_date.text = (context.applicationContext as BaseApplication).
-                dependencyRetriever!!.dateFormatter.format(article.publishedAt)
+        article_date.text = (context.applicationContext as BaseApplication).dependencyRetriever!!.dateFormatter.format(article.publishedAt)
     }
 
     override fun onDestroyView() {
